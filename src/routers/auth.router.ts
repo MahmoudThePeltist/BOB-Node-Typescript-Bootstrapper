45bde9e463771effc9af78406989b5c24b54c9f8
Mahmoud <mahmoudaburascea@gmail.com>
2021-09-25 00:08:35 +0200

    MODIFICAITON: new 2fa endpoints.

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -12,5 +12,11 @@ let qufl = new Qufl({ secret: process.env.JWT_SECRET });
 let controller = new AuthController(model, qufl);
 
 AuthRouter.post('/login', (req, res, next) => controller.login(req, res, next));
+AuthRouter.post('/login/validate', (req, res, next) => controller.validate(req, res, next));
+
+AuthRouter.get('/me', qufl.auth(), (req, res, next) => controller.activeUser(req, res, next));
+AuthRouter.post('/2fa/enable', qufl.auth(), (req, res, next) => controller.enable2FA(req, res, next));
+AuthRouter.post('/2fa/confirm', qufl.auth(), (req, res, next) => controller.confirm2FA(req, res, next));
+AuthRouter.post('/2fa/disable', qufl.auth(), (req, res, next) => controller.disable2FA(req, res, next));
 
 export default AuthRouter;
