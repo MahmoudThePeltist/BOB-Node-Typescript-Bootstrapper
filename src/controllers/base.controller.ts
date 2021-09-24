6703f909cf962fa5bbf9ab9573f12bd7c91687cc
Mahmoud <mahmoudaburascea@gmail.com>
2021-09-25 00:29:37 +0200

    FEATURE: added count, filtering and mass deletion functions.

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,16 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseControllerInterface } from "../interfaces/controller.interface";
 import { BaseModelInterface } from "../interfaces/model.interface";
+import { FilterObject, filterQueryBuilder } from "../utilities/search.utils";
 
 export default class BaseController<T> implements BaseControllerInterface<T> {
+
+    protected options: {
+        filters?: FilterObject[],
+        andMode?: boolean,
+        searchAllAttribute?: string
+    } = {}
 
     constructor(private model: BaseModelInterface<T>) {}
 
     async getAll(req: Request, res: Response, next: NextFunction) {
         try {
-            let response = await this.model.get();
+            const query = filterQueryBuilder(req.query, this.options?.filters, this.options?.andMode, this.options?.searchAllAttribute);
+            
+            const page: any = req?.query?.page ? Number(req?.query?.page) : undefined;
+            const pageSize: any = req?.query?.pageSize ? Number(req?.query?.pageSize) : 10;
+            
+            let response = await this.model.get(query, page, pageSize);
             res.send({
-                data: response,
+                ...response,
                 success: true
             })
         } catch(e) {
@@ -22,7 +34,7 @@ export default class BaseController<T> implements BaseControllerInterface<T> {
         try {
             let response = await this.model.get({_id: req.params.id});
             res.send({
-                data: response,
+                ...response,
                 success: true
             })
         } catch(e) {
@@ -60,7 +72,31 @@ export default class BaseController<T> implements BaseControllerInterface<T> {
 
     async delete(req: Request, res: Response, next: NextFunction) {
         try {
-            let response = await this.model.delete({_id: req.params.id});
+            let response = await this.model.delete(req.params.id);
+            res.send({
+                data: response,
+                success: true
+            })
+        } catch(e) {
+            next(e);
+        }
+    }
+
+    async deleteMany(req: Request, res: Response, next: NextFunction) {
+        try {
+            let response = await this.model.delete(req.body.data);
+            res.send({
+                data: response,
+                success: true
+            })
+        } catch(e) {
+            next(e);
+        }
+    }
+
+    async count(req: Request, res: Response, next: NextFunction) {
+        try {
+            let response = await this.model.count({});
             res.send({
                 data: response,
                 success: true
