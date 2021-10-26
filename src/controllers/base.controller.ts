d30e408281f00926aefc8beee1ad41bf7e030a60
Mahmoud <mahmoudaburascea@gmail.com>
2021-10-26 12:18:52 +0200

    FEATURE: new controller structure based on new prisma model

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -15,12 +15,10 @@ export default class BaseController<T> implements BaseControllerInterface<T> {
 
     async getAll(req: Request, res: Response, next: NextFunction) {
         try {
-            const query = filterQueryBuilder(req.query, this.options?.filters, this.options?.andMode, this.options?.searchAllAttribute);
-            
             const page: any = req?.query?.page ? Number(req?.query?.page) : undefined;
             const pageSize: any = req?.query?.pageSize ? Number(req?.query?.pageSize) : 10;
             
-            let response = await this.model.get(query, page, pageSize);
+            let response = await this.model.get({}, page, pageSize);
             res.send({
                 ...response,
                 success: true
@@ -32,7 +30,7 @@ export default class BaseController<T> implements BaseControllerInterface<T> {
 
     async getSpecific(req: Request, res: Response, next: NextFunction) {
         try {
-            let response = await this.model.get({_id: req.params.id});
+            let response = await this.model.get({id: req.params.id});
             res.send({
                 ...response,
                 success: true
@@ -44,7 +42,8 @@ export default class BaseController<T> implements BaseControllerInterface<T> {
 
     async create(req: Request, res: Response, next: NextFunction) {
         try {
-            const data: Partial<T> = req.body;
+            // Create one or many
+            const data: Partial<T> | Partial<T>[] = req.body;
 
             let response = await this.model.create(data);
             res.send({
@@ -60,7 +59,35 @@ export default class BaseController<T> implements BaseControllerInterface<T> {
         try {
             const data: Partial<T> = req.body;
 
-            let response = await this.model.update({_id: req.params.id}, data);
+            let response = await this.model.update({id: req.params.id}, data);
+            res.send({
+                data: response,
+                success: true
+            })
+        } catch(e) {
+            next(e);
+        }
+    }
+
+    async updateMany(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data: Partial<T> = req.body;
+
+            let response = await this.model.updateMany({id: req.params.id}, data);
+            res.send({
+                data: response,
+                success: true
+            })
+        } catch(e) {
+            next(e);
+        }
+    }
+
+    async upsert(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data: Partial<T> = req.body;
+
+            let response = await this.model.upsert({id: req.params.id}, data);
             res.send({
                 data: response,
                 success: true
